9857cf5afcda1ee6cbd4c00aac515a86dcfa089a
Dani Bassett <dani@danithetallgirl.com>
2020-07-11 22:26:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,6 @@
-require 'prime'
+#require 'prime'
 
 def prime?(num)
   num.prime? do |num|
   end
 end
-
-#prime?(1..100)
-
-#def long_planeteer_calls(calls)
- # calls.any? do |calls|
-  #  calls.length > 4
-  #end
-#end
